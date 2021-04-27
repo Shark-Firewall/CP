60a9e6af4471d5a64b31b78d36f18f2e854ca4a9
Rajnish Kumar <Kumarrajnish363.rk@gmail.com>
2021-04-27 14:45:17 +0530

    Codeforces

--- Sum of Round Numbers.cpp
+++ Sum of Round Numbers.cpp
@@ -1,25 +1,35 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define test() ll t;cin>>t;while(t--)
 #define endl '\n'
-
+#define int long long
+#define pb push_back
+#define vi vector<int>
+#define mod 1000000007
+#define test() int t;cin>>t;while(t--)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    test(){
-        ll a;
-        cin>>a;
-        std::vector<ll> v(a);
-        for(auto i=0;i<a;i++) cin>>v[i];
-        sort(v.begin(), v.end());
-        bool ans=true;
-        for(auto i=1;i<a;i++){
-            ans &= (v[i]-v[i-1]<=1);
+int32_t main(){
+    FIO;
+    test()
+    {
+        int n;
+        cin>>n;
+        vi v;
+        int p=1;
+        while(n)
+        {
+            if(n%10){
+                v.pb((n%10)*p);
+            }
+            n/=10; p*=10;
         }
-        if(ans) cout<<"YES"<<endl;
-        else cout<<"NO"<<endl;
+
+        cout<<v.size()<<endl;
+        for(auto pr:v){
+            cout<<pr<<" ";
+        }
+        cout<<endl;
     }
     return 0;
 }
