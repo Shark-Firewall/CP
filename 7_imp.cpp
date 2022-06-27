20ad1b279bd2af3e84430dc3102c2803f201edd2
Rajnish Kumar <Kumarrajnish363.rk@gmail.com>
2022-06-28 00:06:23 +0530

    cp_sheet

---
+++ 7_imp.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define int long long
+#define vi  vector<int>
+#define pb  push_back
+#define mod 1000000007
+#define test() int t;cin>>t;while(t--)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+int32_t main(){
+    FIO;
+    int n,m;
+    cin>>n>>m;
+    int count=0;
+    for(int i=1;i<=n;i++){
+        if(i%4==2){
+            for(int j=1;j<m;j++) cout<<".";
+            cout<<"#"<<endl;;
+        }
+        else if(i%4==0){
+            cout<<"#";
+            for(int j=2;j<=m;j++) cout<<".";
+            cout<<endl;
+        }
+        else{
+            for(int j=1;j<=m;j++){
+                cout<<"#";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
