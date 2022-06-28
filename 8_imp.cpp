a5878a57d05ae5d1b8b098e2be55fafc5ec10770
Rajnish Kumar <Kumarrajnish363.rk@gmail.com>
2022-06-28 16:31:53 +0530

    cp_sheet

---
+++ 8_imp.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define int long long
+#define vi  vector<int>
+#define pb  push_back
+#define mod 1000000007
+#define test() int t;cin>>t;while(t--)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+int32_t main(){
+    FIO;
+    map<string,int>mp;
+    mp["Tetrahedron"]=4;
+    mp["Cube"]=6;
+    mp["Octahedron"]=8;
+    mp["Dodecahedron"]=12;
+    mp["Icosahedron"]=20;
+    int ans=0;
+    test(){
+    	string s;cin>>s;
+		ans+=mp[s];
+    }
+    cout<<ans<<endl;
+    return 0;
+}
