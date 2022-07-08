69e9ccec9be163ceb7f66544900711fd14bced02
Rajnish Kumar <Kumarrajnish363.rk@gmail.com>
2022-07-09 00:10:50 +0530

    dp

--- Bytelandian_gold_coins.cpp
+++ Bytelandian_gold_coins.cpp
@@ -12,25 +12,19 @@
 #define FIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
 
-
 unordered_map<int,int>dp;
 
-int solve(int amount,vector<int>& coins){
-	if(amount==0) return dp[0]=0;
-	if(dp.count(amount)) return dp[amount];
-	int ans=INT_MAX;
-	for(int coin:coins){
-		if((amount-coin)>=0)
-	        ans=min(ans,solve((amount-coin),coins)+1);
-	}
-	return dp[amount]=ans;
+int solve(int n){
+	if(n<=2) return dp[n]=n;
+	if(dp.count(n)) return dp[n];
+    return dp[n]=max(n,solve(n/2)+solve(n/3)+solve(n/4));
 }
 
 int32_t main(){
     FIO;
-    vector<int> coins={1,5,10,20,100};
-    int n;cin>>n; 
-    int ans=solve(n,coins);
-    cout<<ans<<endl;
+    int n;
+    while(cin>>n){
+    	cout<<solve(n)<<endl;
+    }
     return 0;
 }
