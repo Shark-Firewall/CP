8c4a92c2866b3e71b5df7f38c880f281dfbd6109
Rajnish Kumar <Kumarrajnish363.rk@gmail.com>
2022-07-09 10:54:49 +0530

    dp

--- Frog_2.cpp
+++ Frog_2.cpp
@@ -14,21 +14,21 @@ using namespace std;
 const int N=1e5+10;
 vi dp(N,-1);
 
-int solve(int n,vi &v){
+int solve(int n,int K,vi &v){
 	int cost =INT_MAX;
 	if(dp[n]!=-1) return dp[n];
 	if(n==0) return 0;
-	cost = min(cost,solve(n-1,v)+abs(v[n]-v[n-1]));
-	if(n>1)
-		cost = min(cost,solve(n-2,v)+abs(v[n]-v[n-2]));
+	for(int i=1;i<=K;i++)
+	    if(n-i>=0)
+			cost = min(cost,solve(n-i,K,v)+abs(v[n]-v[n-i]));
 	return dp[n]=cost;
 }
 
 int32_t main(){
     FIO;
-    int n;cin>>n; 
+    int n,k;cin>>n>>k; 
     vi v(n);
     for(int i=0;i<n;i++) cin>>v[i];
-    cout<<solve(n-1,v)<<endl;
+    cout<<solve(n-1,k,v)<<endl;
     return 0;
 }
